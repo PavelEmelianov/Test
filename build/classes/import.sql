38a7b1d3452346d58b1e656bdcc14b6bc78c37bb
PavelEmelianovd <emelianov@emelianov-lx.nixsolutions.com>
2016-01-20 15:16:09 +0200

    asdf

--- build/classes/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Role VALUES (default,'admin');
-INSERT INTO Role VALUES (default,'user');
-
-INSERT INTO User(id,login,password,email,firstName,lastName,birthDate,roleId) VALUES (default,'admin','admin','admin@mail.ru','Ivan','Ivanov','1990-10-10',1);
-INSERT INTO User(id,login,password,email,firstName,lastName,birthDate,roleId) VALUES (default,'user','user','user@mail.ru','Petr','Petrov','1989-06-06',2);
-
-
