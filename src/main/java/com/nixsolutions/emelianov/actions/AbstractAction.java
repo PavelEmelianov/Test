c44d852e4aa7db991c0fbaf7f7301b6bb964f055
Pavel Emelianov <emelianov-p@mail.ru>
2016-01-21 10:14:13 +0200

    comment added

--- src/main/java/com/nixsolutions/emelianov/actions/AbstractAction.java
+++ src/main/java/com/nixsolutions/emelianov/actions/AbstractAction.java
@@ -1,5 +1,5 @@
 package com.nixsolutions.emelianov.actions;
-
+//abstract action
 import javax.inject.Inject;
 import org.springframework.beans.factory.annotation.Qualifier;
 import com.nixsolutions.emelianov.service.RoleService;
